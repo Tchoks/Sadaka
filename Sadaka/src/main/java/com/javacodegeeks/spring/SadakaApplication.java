0d621742db23b7948635fefed4d1e45f501f12b3
MarLeo <martintchokonthe@yahoo.fr>
2016-04-30 12:56:40 +0200

    First commit

---
+++ Sadaka/src/main/java/com/javacodegeeks/spring/SadakaApplication.java
@@ -0,0 +1,12 @@
+package com.javacodegeeks.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadakaApplication {   
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadakaApplication.class, args);
+	}
+}
